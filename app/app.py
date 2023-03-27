5b8a06982f80b4a0cef02b9cceae606279811a74
domiherriott <dherriott@deloitte.co.uk>
2023-03-27 17:52:40 +0100

    tweaks

--- app/app.py
+++ app/app.py
@@ -8,6 +8,11 @@ from dash import Dash, html, dcc
 import plotly.express as px
 import plotly.graph_objects as go
 
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
+
 app = Dash(__name__)
 
 counties_filename = 'data/Counties_and_Unitary_Authorities_transformed.geojson'
@@ -45,29 +50,41 @@ fig = go.Figure(go.Choroplethmapbox(
     geojson=counties,
     locations=df['id'], 
     z=df[z_col], # z=df.unemp,
+    text=df['name'],
     colorscale="Viridis", 
     zmin=df[z_col].min(),
     zmax=df[z_col].max(),
     marker_opacity=0.5,
     marker_line_width=0)
 )
-fig.update_layout(mapbox_style="carto-positron",
-                  mapbox_zoom=4.4,
-                  mapbox_center = {"lat": 54.093409, "lon": -2.89479},
-                  height=500)
+fig.update_layout(
+    title_text='',
+    mapbox_style="carto-positron",
+    mapbox_zoom=4.4,
+    mapbox_center = {"lat": 54.093409, "lon": -2.89479},
+    height=500
+)
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    # html.H1(children='Hello Dash',
+    #         style={
+    #         'textAlign': 'center',
+    #         'color': colors['text'],
+    #         'height':'10vh'
+    #     }),
 
-    html.Div(children='''
-        Dash: A web application framework for your data.
-    '''),
+    # html.Div(children='''
+    #     Dash: A web application framework for your data.
+    # '''),
 
     dcc.Graph(
         id='example-graph',
-        figure=fig
+        figure=fig,
+        style={
+            'height':'100vh'
+        }
     )
 ])
 
